a515e89d344e5331e870ba0e18055482881f1633
Cyril Gadal <cyril.gadal@gmail.com>
2023-04-27 15:55:32 +0200

    Adding figure for parameter space

---
+++ figure_scripts/figure2.py
@@ -0,0 +1,57 @@
+import glob
+import os
+import sys
+
+import matplotlib.pyplot as plt
+import numpy as np
+import template as tp
+from netCDF4 import Dataset
+
+# plt.rcParams['figure.constrained_layout.hspace'] = 0
+# plt.rcParams['figure.constrained_layout.h_pad'] = 0.0005
+
+
+# %% Load data
+path_data = '../data/output_data'
+list_runs = glob.glob(os.path.join(path_data, '*.nc'))
+datasets = np.array([Dataset(run) for run in list_runs])
+
+# %% create data vectors
+alpha, phi, St, Re, = np.array([[d.variables['alpha'][:].data, d.variables['phi'][:].data,
+                                d.variables['St'][:].data, d.variables['Re'][:].data]
+                               for d in datasets]).T
+
+authors = np.array([d.author for d in datasets])
+
+# %% graphic specifications
+# changing order depending on authors
+author_zorder = ['Cyril', 'Cyril/Marie', 'Jean', 'Rastello', 'Julien']
+
+figsize = (tp.half_figure_width, 2.4*tp.half_figure_width)
+fig, axarr = plt.subplots(4, 1, figsize=figsize, constrained_layout=True,
+                          gridspec_kw={'height_ratios': [0.3, 1, 1, 1]})
+
+for ax, var in zip(axarr[1:].flatten(), [phi, alpha, St]):
+    for author in author_zorder:
+        mask = (authors == author)
+        marker = 's' if author == 'Julien' else None
+        ax.scatter(Re[mask], var[mask], c=tp.color_setups[author],
+                   label=author, marker=marker)
+        ax.set_xscale('log')
+        ax.set_yscale('log')
+
+axarr[0].axis('off')
+handles, labels = axarr[1].get_legend_handles_labels()
+leg = axarr[0].legend(handles, labels, loc="upper center", ncol=3,
+                      borderaxespad=0, title='Datasets', mode='expand')
+#
+axarr[1].set_xticks([])
+axarr[2].set_xticks([])
+axarr[3].set_xlabel(r'Reynolds number, $\mathcal{R}e$')
+#
+axarr[1].set_ylabel(r'Volume fraction, $\phi$')
+axarr[2].set_ylabel(r'angle, $\alpha~[^\circ]$')
+axarr[3].set_ylabel(r'Stokes number, $\mathcal{S}t$')
+#
+axarr[2].set_yscale('linear')
+axarr[2].set_ylim(bottom=-1)
